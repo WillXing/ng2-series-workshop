744899edf479620e64b19279272ccf2585a430c8
Xing <yellowstar1992@hotmail.com>
2016-09-22 11:21:25 +0800

    move appbar to app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import {Component} from "@angular/core";
-
 import {ROUTER_DIRECTIVES} from "@angular/router";
+
+import {AppBar} from "./shared/components";
 
 @Component({
   selector: 'app',
   template: require('./app.html'),
   directives: [
+    AppBar,
     ...ROUTER_DIRECTIVES
   ]
 })
