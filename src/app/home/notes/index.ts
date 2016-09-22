0f5de456b23023c70af241e4e3f4d5fc7f3b4813
Xing <yellowstar1992@hotmail.com>
2016-09-22 09:06:20 +0800

    connect api in note component

--- src/app/home/notes/index.ts
+++ src/app/home/notes/index.ts
@@ -1,6 +1,7 @@
 import {Component} from "@angular/core";
 import {NoteCard} from "./note-card"
 import { NoteCreator } from './note-creator'
+import {NoteService} from "../../shared/services/nodes";
 
 @Component({
   selector: 'notes',
@@ -9,23 +10,23 @@ import { NoteCreator } from './note-creator'
   directives: [NoteCard, NoteCreator]
 })
 export class Notes {
-  notes = [
-    {
-      title: 'n',
-      value: 'v',
-      color: 'rgb(255, 209, 128)'
-    }, {
-      title: 'n1',
-      value: 'v1',
-      color: 'rgb(204, 255, 144)'
-    }
-  ]
+  notes = []
 
-  checkCard(note, i) {
-    this.notes.splice(i, 1)
+  constructor(private noteService: NoteService) {
+    this.noteService.getNotes().subscribe(res => this.notes = res.data)
+  }
+
+  checkCard(note) {
+    this.noteService.completeNote(note)
+      .subscribe(note => {
+        const i = this.notes.findIndex(localNote => localNote.id === note.id)
+        this.notes.splice(i, 1)
+      })
   }
 
   addNote(note) {
-    this.notes.push(note);
+    console.log(note)
+    this.noteService.createNote(note)
+      .subscribe(note => this.notes.push(note))
   }
 }
